d3c9b5a71c907136fece19304a318d2a1923bbca
MarcioCasa <mssantostuc@gmail.com>
2020-05-11 18:29:17 -0300

    aula 20

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -2,7 +2,7 @@
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Usuario
+    public class Usuario : Entidade
     {
         public int Id { get; set; }
 
@@ -18,5 +18,10 @@ namespace QuickBuy.Dominio.Entidades
         /// Um Usuario pode ter nenhum ou muitos pedidos
         /// </summary>
         public ICollection<Pedido> Pedidos { get; set; }
+
+        public override void Validate()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 } 
